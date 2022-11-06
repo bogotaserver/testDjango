49f7c2adb2df9709697037eb222072e4ef99784f
Ian <ian.hoefsloot@hotmail.com>
2022-11-06 23:34:26 +0100

    model voetbalspelers toegevoegd en admin bijgewerkt

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Voetbalspelers
 
 
 admin.site.register(Post)
+admin.site.register(Voetbalspelers)
 
 
 #Hier is nu het model Post zichtbaar en bewerkbaar gemaakt in de admin ruimte.
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Voetbalspelers(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    huidige_club = models.CharField(max_length=40)
+    naam_voetballer = models.CharField(max_length=30)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.naam_voetballer
